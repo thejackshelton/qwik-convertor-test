a9dc4caf6082b42a05f56e5ee51d91b7770cb273
thejackshelton <me@jackshelton.com>
2025-06-25 15:31:20 -0500

    tailwind classes

--- src/routes/tailwind/index.tsx
+++ src/routes/tailwind/index.tsx
@@ -2,6 +2,6 @@ import { component$ } from "@builder.io/qwik";
 
 export default component$(() => {
   return (
-    <div>Tailwind</div>
+    <div class="bg-red-500 text-blue-300 text-2xl font-bold">Tailwind</div>
   )
 });
