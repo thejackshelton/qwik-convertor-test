25a64ce04c0135ffea966de6e537e7dcf018ceed
thejackshelton <me@jackshelton.com>
2025-06-25 16:01:25 -0500

    vanilla css file

--- src/routes/vanilla/index.tsx
+++ src/routes/vanilla/index.tsx
@@ -1,6 +1,10 @@
-import { component$ } from "@builder.io/qwik";
+import { component$, useStyles$ } from "@builder.io/qwik";
+import styles from "./vanilla.css?inline";
 
 export default component$(() => {
+  // this uses our css styles
+  useStyles$(styles);
+
   return (
     <div>Vanilla</div>
   )
